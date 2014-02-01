f48c65dcd3a7bc3b2e256090fb3960a94fb8d928
Florian Frank <flori@ping.de>
2014-02-01 12:04:21 +0100

    Refactor sections method by decomposition

--- lib/betterdocs/global.rb
+++ lib/betterdocs/global.rb
@@ -90,34 +90,37 @@ module Betterdocs
         end
       end
 
-      def sections
-        unless @sections
-          Dir.chdir Rails.root.join('app/controllers') do
-            all_docs = []
-            for cf in Dir[api_prefix.to_s + '/**/*_controller.rb']
-              controller_name = cf.sub(/\.rb$/, '').camelcase
-              controller =
-                begin
-                  controller_name.constantize
-                rescue NameError => e
-                  STDERR.puts "Skipping #{cf.inspect}, #{e.class}: #{e}"
-                  next
-                end
-              if docs = controller.ask_and_send(:docs)
-                all_docs << docs
-              else
-                STDERR.puts "Skipping #{cf.inspect}, #{controller_name.inspect} doesn't respond to :docs method"
-              end
+      def all_docs
+        Dir[api_prefix.to_s + '/**/*_controller.rb'].each_with_object([]) do |cf, all|
+          controller_name = cf.sub(/\.rb$/, '').camelcase
+          controller =
+            begin
+              controller_name.constantize
+            rescue NameError => e
+              STDERR.puts "Skipping #{cf.inspect}, #{e.class}: #{e}"
+              next
             end
-            actions = all_docs.reduce([]) { |a, d| a.concat(d.actions) }
-            actions.each_with_object(@sections = {}) do |action, sections|
-              sections[action.section] ||= Section.new(action.section)
-              sections[action.section] << action
-            end
+          if docs = controller.ask_and_send(:docs)
+            all << docs
+          else
+            STDERR.puts "Skipping #{cf.inspect}, #{controller_name.inspect} doesn't respond to :docs method"
           end
-          @sections.freeze
         end
-        @sections
+      end
+
+      def actions
+        all_docs.reduce([]) { |a, d| a.concat(d.actions) }
+      end
+
+      def sections
+        @sections and return @sections
+        Dir.chdir Rails.root.join('app/controllers') do
+          actions.each_with_object(@sections = {}) do |action, sections|
+            sections[action.section] ||= Section.new(action.section)
+            sections[action.section] << action
+          end
+        end
+        @sections.freeze
       end
 
       def sections_clear
