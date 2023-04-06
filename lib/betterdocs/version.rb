367e57cf767c4bb6425882f2e2fd2db3ba331225
Riccardo Giomi <riccardo.giomi@betterplace.org>
2023-04-06 09:26:34 +0200

    Bump version to 0.12.4.

--- lib/betterdocs/version.rb
+++ lib/betterdocs/version.rb
@@ -1,6 +1,6 @@
 module Betterdocs
   # Betterdocs version
-  VERSION         = '0.12.3'
+  VERSION         = '0.12.4'
   VERSION_ARRAY   = VERSION.split('.').map(&:to_i) # :nodoc:
   VERSION_MAJOR   = VERSION_ARRAY[0] # :nodoc:
   VERSION_MINOR   = VERSION_ARRAY[1] # :nodoc:
